8e012451d902d4b99b71fc94ec9998bb76a91c60
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-01 12:24:25 +0000

    Footer terminado

--- react-lab1/src/components/Footer.jsx
+++ react-lab1/src/components/Footer.jsx
@@ -3,22 +3,25 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer-container">
-      <ul>
-        <li>Contacto</li>
-        <li>Email: email.com</li>
-        <li>Teléfono</li>
-      </ul>
-      <ul>
-        <li>Redes Sociales</li>
-        <li>Facebook</li>
-        <li>Twiter</li>
-        <li>Instagram</li>
-      </ul>
-      <ul>
-        <li>Dirección</li>
-        <li>Calle: Lejana 11</li>
-        <li>Ciudad: País</li>
-      </ul>
+      <div className="footer-details">
+        <ul>
+          <li>Contacto</li>
+          <li>Email: email.com</li>
+          <li>Teléfono</li>
+        </ul>
+        <ul>
+          <li>Redes Sociales</li>
+          <li>Facebook</li>
+          <li>Twiter</li>
+          <li>Instagram</li>
+        </ul>
+        <ul>
+          <li>Dirección</li>
+          <li>Calle: Lejana 11</li>
+          <li>Ciudad: País</li>
+        </ul>
+      </div>
+      <p>© 2023 MiTienda todos los derechos reservados</p>
     </footer>
   );
 }
