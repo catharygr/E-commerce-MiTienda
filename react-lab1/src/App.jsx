720a4d50de6547f8e0cada3690e7f493e32dab25
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-01 12:15:53 +0000

    Trabajando en el componente Footer

--- react-lab1/src/App.jsx
+++ react-lab1/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
