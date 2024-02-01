1037ad9715094d67b41cbd346d405812c6446b45
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-01 13:10:52 +0000

    Terminado el componente ProductCard.css

--- react-lab1/src/components/content/ProductCard.jsx
+++ react-lab1/src/components/content/ProductCard.jsx
@@ -3,10 +3,10 @@ export default function ProductCard({ product }) {
   return (
     <div className="product-card">
       <img src={product.image} alt={product.title} />
-      <div>
-        <h3>{product.title}</h3>
+      <div className="card-body">
+        <h3 className="card-title">{product.title}</h3>
         <p>{product.description}</p>
-        <p className="price">{product.price}€</p>
+        <p className="card-price">{product.price}€</p>
       </div>
     </div>
   );
