34218a12be99536b117e83a1e5d824babb475fed
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-01 12:41:41 +0000

    Terminado el componente Promotion

--- react-lab1/src/App.jsx
+++ react-lab1/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header";
 import "./App.css";
 import Footer from "./components/Footer";
+import Promotion from "./components/content/Promotion";
 
 function App() {
   return (
     <>
       <Header />
+      <Promotion />
       <Footer />
     </>
   );
---
+++ react-lab1/src/components/content/Promotion.jsx
@@ -0,0 +1,9 @@
+import "./Promotion.css";
+
+export default function Promotion() {
+  return (
+    <div className="promotion">
+      <p>¡20% de descuento para nuevos clientes!</p>
+    </div>
+  );
+}
