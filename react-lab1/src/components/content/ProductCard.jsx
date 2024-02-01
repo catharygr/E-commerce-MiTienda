d84a3209be868f214be7c7e446a4a8dda36252ee
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-01 13:54:49 +0000

    Terminado el componente MainContent

--- react-lab1/src/components/content/ProductCard.jsx
+++ react-lab1/src/components/content/ProductCard.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
+import "./ProductCard.css";
+
 export default function ProductCard({ product }) {
   return (
     <div className="product-card">
-      <img src={product.image} alt={product.title} />
+      <img className="card-img" src={product.image} alt={product.title} />
       <div className="card-body">
         <h3 className="card-title">{product.title}</h3>
-        <p>{product.description}</p>
+        <p className="card-description">{product.description}</p>
         <p className="card-price">{product.price}€</p>
       </div>
     </div>
