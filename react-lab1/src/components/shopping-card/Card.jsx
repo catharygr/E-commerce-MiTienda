3776c10f3c2cdca9fc9637175785789f9e1b81b7
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-07 14:00:54 +0000

    Trabajando en la noti

---
+++ react-lab1/src/components/shopping-card/Card.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import "./Card.css";
+
+export default function Card({ product }) {
+  const { image, title, price } = product;
+  return (
+    <div className="shopping-card-container">
+      <div className="notification-cicle-container">
+        <img
+          className="shopping-card-image"
+          src={image}
+          alt={title}
+        />
+        <span>5</span>
+      </div>
+      <div className="shopping-card-body">
+        <p>{title}</p>
+        <p>Precio por unidad: {price}</p>
+        <p>Precio total: $345.20</p>
+      </div>
+    </div>
+  );
+}
--- react-lab1/src/components/shopping-card/ShoppingCart.jsx
+++ react-lab1/src/components/shopping-card/ShoppingCart.jsx
@@ -1,8 +1,14 @@
 import "./ShoppingCart.css";
+import Card from "./Card";
+import data from "../../assets/data.json";
+
 export default function ShoppingCart() {
   return (
-    <div>
-      <h1>Shopping Cart</h1>
-    </div>
+    <main className="shopping-cart-container">
+      <h2>Carrito de Compras</h2>
+      <Card product={data[1]} />
+      <Card product={data[2]} />
+      <Card product={data[3]} />
+    </main>
   );
 }
