b18696ceb07824123de6c9826cd908832a8fcc2a
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-07 14:02:51 +0000

    Trabajando en el componente Card.jsx

--- react-lab1/src/components/shopping-card/ShoppingCart.jsx
+++ react-lab1/src/components/shopping-card/ShoppingCart.jsx
@@ -4,7 +4,7 @@ import data from "../../assets/data.json";
 
 export default function ShoppingCart() {
   return (
-    <main className="shopping-cart-container">
+    <main className="shopping-cart-container notification-cicle">
       <h2>Carrito de Compras</h2>
       <Card product={data[1]} />
       <Card product={data[2]} />
