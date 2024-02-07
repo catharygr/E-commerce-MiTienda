31c8162df542ec80daebf71d8cfa28f073432321
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-07 12:17:24 +0000

    Trabajando en ProductCard.jsx

--- react-lab1/src/components/content/ProductCard.jsx
+++ react-lab1/src/components/content/ProductCard.jsx
@@ -4,12 +4,17 @@ import "./ProductCard.css";
 export default function ProductCard({ product }) {
   return (
     <div className="product-card">
-      <img className="card-img" src={product.image} alt={product.title} />
+      <img
+        className="card-img"
+        src={product.image}
+        alt={product.title}
+      />
       <div className="card-body">
         <h3 className="card-title">{product.title}</h3>
         <p className="card-description">{product.description}</p>
         <p className="card-price">{product.price}€</p>
       </div>
+      <button className="card-btn">Agregar carrito</button>
     </div>
   );
 }
