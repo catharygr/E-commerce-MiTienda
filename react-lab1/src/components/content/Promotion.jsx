c219fe22b79476c02fe6b41d73f27092e55e132a
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-08 10:47:22 +0000

    Trabajando en el componente Promotion.jsx

--- react-lab1/src/components/content/Promotion.jsx
+++ react-lab1/src/components/content/Promotion.jsx
@@ -1,16 +1,22 @@
 import "./Promotion.css";
 import { UserContext } from "../../contextos/UserContext";
+import { RouterContext } from "../../contextos/RouterContext";
 import { useContext } from "react";
 
 export default function Promotion() {
   const { user } = useContext(UserContext);
-  return (
-    <div className="promotion">
-      {!user.isLogged ? (
-        <p>¡20% de descuento para nuevos clientes!</p>
-      ) : (
-        <p>¡{user.name}, aprovéchate de tu 20% de descuento!</p>
-      )}
-    </div>
-  );
+  const { route } = useContext(RouterContext);
+
+  const banner = () => {
+    if (user.isLogged && route === "content") {
+      return <p>¡{user.name} aprovéchate de tu 20% de descuento!</p>;
+    } else if (!user.isLogged && route === "shopping-cart") {
+      return <p>Crea una cuenta para disfrutar de nuestros descuentos</p>;
+    } else if (user.isLogged && route === "shopping-cart") {
+      return <p>{user.name} los 20% se aplicarán al final de la compra</p>;
+    } else {
+      return <p>¡20% de descuento para nuevos clientes!</p>;
+    }
+  };
+  return <div className="promotion">{banner()}</div>;
 }
