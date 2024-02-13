64967a4962121f0348d58e41d837750a7ebd6de3
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-13 15:47:14 +0000

    Trabajando en  LoginForm.jsx

---
+++ react-lab1/src/components/promotion/Promotion.jsx
@@ -0,0 +1,21 @@
+import "./Promotion.css";
+import { UserContext } from "../../contextos/UserContext";
+import { useContext } from "react";
+
+export default function Promotion() {
+  const { user } = useContext(UserContext);
+  const route = window.location.pathname.split("/")[1];
+
+  const banner = () => {
+    if (user.isLogged && route === "/") {
+      return <p>¡{user.name} aprovéchate de tu 20% de descuento!</p>;
+    } else if (!user.isLogged && route === "cart") {
+      return <p>Crea una cuenta para disfrutar de nuestros descuentos</p>;
+    } else if (user.isLogged && route === "cart") {
+      return <p>{user.name} los 20% se aplicarán al final de la compra</p>;
+    } else {
+      return <p>¡20% de descuento para nuevos clientes!</p>;
+    }
+  };
+  return <div className="promotion">{banner()}</div>;
+}
