ca1e1ecfba5cf91c805136758c5c88fafc1badc0
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-13 11:42:36 +0000

    Instalación de react-router-dom

--- react-lab1/src/main.jsx
+++ react-lab1/src/main.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./main.css";
 import UserProvider from "./contextos/UserContext.jsx";
-import RouterProvider from "./contextos/RouterContext.jsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
-      <RouterProvider>
-        <App />
-      </RouterProvider>
+      <RouterProvider router={router} />
     </UserProvider>
   </React.StrictMode>
 );
