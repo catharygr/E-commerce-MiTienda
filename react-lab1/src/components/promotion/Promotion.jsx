443a3a8e252bf36fdee5e2cb37c85d24d6b8b108
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-14 10:56:13 +0000

    Trabajando en Promotion.jsx

--- react-lab1/src/components/promotion/Promotion.jsx
+++ react-lab1/src/components/promotion/Promotion.jsx
@@ -10,17 +10,16 @@ export default function Promotion() {
   const route = location.pathname.split("/")[1];
 
   const banner = () => {
-    if (user.isLogged && route === "/") {
-      return <p>{!user.name}, aprovéchate de tu 20% de descuento!</p>;
+    if (user.isLogged && route === "") {
+      return <p>{user.name}, aprovéchate de tu 20% de descuento!</p>;
     } else if (!user.isLogged && route === "login") {
       return <p>Inicia sesión para disfrutar de nuestros descuentos</p>;
-    } else if (!user.isLogged && route === "cart") {
-      return <p>Crea una cuenta para disfrutar de nuestros descuentos</p>;
     } else if (user.isLogged && route === "cart") {
       return <p>{user.name}, los 20% se aplicarán al final de la compra</p>;
     } else {
       return <p>¡20% de descuento para nuevos clientes!</p>;
     }
   };
+
   return <div className="promotion">{banner()}</div>;
 }
