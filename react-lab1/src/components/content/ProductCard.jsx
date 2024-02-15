bec7857cc0f1bc9aa9c4f20c15a01ac8b91374a6
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-15 12:02:06 +0000

    Trabajando en ProductCard.css

--- react-lab1/src/components/content/ProductCard.jsx
+++ react-lab1/src/components/content/ProductCard.jsx
@@ -7,35 +7,36 @@ import { Link } from "react-router-dom";
 export default function ProductCard({ product }) {
   const { user, setUser } = useContext(UserContext);
 
-  const handleAddToCart = () => {
+  const handleAddToCart = (e) => {
+    e.stopPropagation();
     setUser({
       ...user,
       shoppingCartItems: [...user.shoppingCartItems, product.id],
     });
   };
   return (
-    <div className="product-card">
-      <Link to={`/product/${product.id}`}>
+    <Link to={`/product/${product.id}`}>
+      <div className="product-card">
         <img
           className="card-img"
           src={product.image}
           alt={product.title}
         />
-      </Link>
-      <div className="card-body">
-        <h3 className="card-title">{product.title}</h3>
-        <p className="card-description">{product.description}</p>
-        <p className="card-price">{product.price}€</p>
-      </div>
+        <div className="card-body">
+          <h3 className="card-title">{product.title}</h3>
+          <p className="card-description">{product.description}</p>
+          <p className="card-price">{product.price}€</p>
+        </div>
 
-      {user.isLogged && (
-        <button
-          onClick={handleAddToCart}
-          className="card-btn"
-        >
-          Agregar carrito
-        </button>
-      )}
-    </div>
+        {user.isLogged && (
+          <button
+            onClick={handleAddToCart}
+            className="card-btn"
+          >
+            Agregar carrito
+          </button>
+        )}
+      </div>
+    </Link>
   );
 }
