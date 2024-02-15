09948044687ca62c80ecc670f4fb4c153577d353
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-15 12:34:23 +0000

    Trabajando en la lógica del componente ProductCard.jsx

--- react-lab1/src/components/content/ProductCard.jsx
+++ react-lab1/src/components/content/ProductCard.jsx
@@ -2,10 +2,11 @@
 import "./ProductCard.css";
 import { UserContext } from "../../contextos/UserContext";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function ProductCard({ product }) {
   const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
   const handleAddToCart = (e) => {
     e.stopPropagation();
@@ -14,29 +15,35 @@ export default function ProductCard({ product }) {
       shoppingCartItems: [...user.shoppingCartItems, product.id],
     });
   };
-  return (
-    <Link to={`/product/${product.id}`}>
-      <div className="product-card">
-        <img
-          className="card-img"
-          src={product.image}
-          alt={product.title}
-        />
-        <div className="card-body">
-          <h3 className="card-title">{product.title}</h3>
-          <p className="card-description">{product.description}</p>
-          <p className="card-price">{product.price}€</p>
-        </div>
 
-        {user.isLogged && (
-          <button
-            onClick={handleAddToCart}
-            className="card-btn"
-          >
-            Agregar carrito
-          </button>
-        )}
+  const handleCardClick = () => {
+    navigate(`/product/${product.id}`);
+  };
+
+  return (
+    <div
+      onClick={handleCardClick}
+      className="product-card"
+    >
+      <img
+        className="card-img"
+        src={product.image}
+        alt={product.title}
+      />
+      <div className="card-body">
+        <h3 className="card-title">{product.title}</h3>
+        <p className="card-description">{product.description}</p>
+        <p className="card-price">{product.price}€</p>
       </div>
-    </Link>
+
+      {user.isLogged && (
+        <button
+          onClick={handleAddToCart}
+          className="card-btn"
+        >
+          Agregar carrito
+        </button>
+      )}
+    </div>
   );
 }
