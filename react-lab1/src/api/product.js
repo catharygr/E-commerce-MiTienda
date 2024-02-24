4c834ff5ca103b00280153ad9d0268e4930cb52f
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-24 12:39:22 +0000

    Trabajando con product.js

--- react-lab1/src/api/product.js
+++ react-lab1/src/api/product.js
@@ -8,7 +8,28 @@ import {
 
 const API_URL = "http://localhost:3000/products";
 
-export const addProductAction = (product) => {};
+export const addProductAction = (newProduct) => async (dispatch) => {
+  try {
+    await axios.post(API_URL, newProduct);
+    dispatch({
+      type: PRODUCTS_ADD_PRODUCT,
+      payload: newProduct,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 export const removeProductAction = (id) => {};
 export const updateProductAction = (id) => {};
-export const getProductsAction = () => {};
+
+export const getProductsAction = () => async (dispatch) => {
+  try {
+    const { data } = await axios.get(API_URL);
+    dispatch({
+      type: PRODUCTS_GET_PRODUCTS,
+      payload: data,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
--- react-lab1/src/custom-hooks/useProductActions.jsx
+++ react-lab1/src/custom-hooks/useProductActions.jsx
@@ -1,4 +1,6 @@
 import { useDispatch } from "react-redux";
+import { useState } from "react";
+
 import {
   addProductAction,
   removeProductAction,
@@ -7,11 +9,39 @@ import {
 } from "../api/product.js";
 
 export default function useProductActions() {
-  const addProductMiddleware = () => {};
+  const dispatch = useDispatch();
+  const [isLoading, isSetLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const addProductMiddleware = async (newProduct) => {
+    isSetLoading(true);
+    try {
+      isSetLoading(true);
+      setError(null);
+      await dispatch(addProductAction(newProduct));
+    } catch (error) {
+      setError(error);
+    } finally {
+      isSetLoading(false);
+    }
+  };
   const removeProductMiddleware = () => {};
   const updateProductMiddleware = () => {};
-  const getProductsMiddleware = () => {};
+
+  const getProductsMiddleware = async () => {
+    isSetLoading(true);
+    setError(null);
+    try {
+      await dispatch(getProductsAction());
+    } catch (error) {
+      setError(error);
+    } finally {
+      isSetLoading(false);
+    }
+  };
   return {
+    isLoading,
+    error,
     addProductMiddleware,
     removeProductMiddleware,
     updateProductMiddleware,
