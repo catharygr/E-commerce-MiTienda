3f12e9417ca9acd30dc41cc4138a73337554bf35
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-24 11:18:32 +0000

    Trabajando con la API

---
+++ react-lab1/src/api/product.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import {
+  PRODUCTS_ADD_PRODUCT,
+  PRODUCTS_DELETE_PRODUCT,
+  PRODUCTS_UPDATE_PRODUCT,
+  PRODUCTS_GET_PRODUCTS,
+} from "../redux/actions/actionTypes";
---
+++ react-lab1/src/custom-hooks/useProductActions.jsx
@@ -0,0 +1,12 @@
+export default function useProductActions() {
+  const addProductMiddleware = () => {};
+  const removeProductMiddleware = () => {};
+  const updateProductMiddleware = () => {};
+  const getProductsMiddleware = () => {};
+  return {
+    addProductMiddleware,
+    removeProductMiddleware,
+    updateProductMiddleware,
+    getProductsMiddleware,
+  };
+}
