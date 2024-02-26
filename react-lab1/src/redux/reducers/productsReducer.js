53edd3a1c297ae70e71466ec6c67cc90458b5122
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-26 15:29:27 +0000

    Trabajando von createSlice

--- react-lab1/src/redux/reducers/productsReducer.js
+++ react-lab1/src/redux/reducers/productsReducer.js
@@ -1,61 +1,88 @@
-import {
-  PRODUCTS_ADD_PRODUCT,
-  PRODUCTS_DELETE_PRODUCT,
-  PRODUCTS_UPDATE_PRODUCT,
-  PRODUCTS_GET_PRODUCTS,
-} from "../actions/actionTypes";
+// import {
+//   PRODUCTS_ADD_PRODUCT,
+//   PRODUCTS_DELETE_PRODUCT,
+//   PRODUCTS_UPDATE_PRODUCT,
+//   PRODUCTS_GET_PRODUCTS,
+// } from "../actions/actionTypes";
+
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   products: null,
 };
 
-function productsReducer(state = initialState, action) {
-  switch (action.type) {
-    case PRODUCTS_ADD_PRODUCT:
-      return {
-        ...state,
-        products: {
-          ...state.products,
-          products: [...state.products.products, action.payload],
-        },
-      };
-    case PRODUCTS_DELETE_PRODUCT:
-      return {
-        ...state,
-        products: {
-          ...state.products,
-          products: state.products.products.filter(
-            (product) => product.id !== action.payload
-          ),
-        },
-      };
-    case PRODUCTS_UPDATE_PRODUCT:
-      return {
-        ...state,
-        products: {
-          ...state.products,
-          products: state.products.products.map((product) => {
-            if (product.id === action.payload.id) {
-              return action.payload;
-            }
-            return product;
-          }),
-        },
-      };
-    case PRODUCTS_GET_PRODUCTS:
-      return {
-        ...state,
-        products: {
-          ...state.products,
-          products: action.payload,
-        },
-      };
+// Slice
 
-    default:
-      return state;
-  }
-}
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      state.products.products.push(action.payload);
+    },
+    deleteProduct: (state, action) => {
+      state.products.products = state.products.products.filter(
+        (product) => product.id !== action.payload
+      );
+    },
+    updateProduct: (state, action) => {
+      state.products.products = state.products.products.map((product) =>
+        product.id === action.payload.id ? action.payload : product
+      );
+    },
+    getProducts: (state, action) => {
+      state.products.products = action.payload;
+    },
+  },
+});
 
-export default productsReducer;
+// function productsReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case PRODUCTS_ADD_PRODUCT:
+//       return {
+//         ...state,
+//         products: {
+//           ...state.products,
+//           products: [...state.products.products, action.payload],
+//         },
+//       };
+//     case PRODUCTS_DELETE_PRODUCT:
+//       return {
+//         ...state,
+//         products: {
+//           ...state.products,
+//           products: state.products.products.filter(
+//             (product) => product.id !== action.payload
+//           ),
+//         },
+//       };
+//     case PRODUCTS_UPDATE_PRODUCT:
+//       return {
+//         ...state,
+//         products: {
+//           ...state.products,
+//           products: state.products.products.map((product) => {
+//             if (product.id === action.payload.id) {
+//               return action.payload;
+//             }
+//             return product;
+//           }),
+//         },
+//       };
+//     case PRODUCTS_GET_PRODUCTS:
+//       return {
+//         ...state,
+//         products: {
+//           ...state.products,
+//           products: action.payload,
+//         },
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
+
+export default productsSlice.reducer;
 
 export const getAllProducts = (state) => state.products.products;
