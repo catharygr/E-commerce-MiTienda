1d45f428b5e8f6469e1dbb51dbbc2c50f08883ba
catharygr <103951447+catharygr@users.noreply.github.com>
2024-02-28 11:33:20 +0000

    Lab terminado

--- react-lab1/src/api/product.js
+++ react-lab1/src/api/product.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/products";
+const API_URL = "http://localhost:3000/products";
 
 export const addProductMiddleware = async (newProduct) => {
   try {
