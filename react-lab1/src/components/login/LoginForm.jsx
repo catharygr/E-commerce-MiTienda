1ae9f84bb761f4921bc5dc2a083c0b8d0c139844
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-01 12:02:59 +0000

    Trabajando en LoginForm.jsx

--- react-lab1/src/components/login/LoginForm.jsx
+++ react-lab1/src/components/login/LoginForm.jsx
@@ -1,68 +1,105 @@
 import "./LoginForm.css";
 import { useContext } from "react";
 import { UserContext } from "../../contextos/UserContext";
-import useForm from "../../custom-hooks/useForm";
+// import useForm from "../../custom-hooks/useForm";
 import { useNavigate, useLocation } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export default function LoginForm() {
   const { user, setUser } = useContext(UserContext);
-  const { form, setName, setEmail, reset } = useForm();
+  // const { form, setName, setEmail, reset } = useForm();
   const navigate = useNavigate();
   const location = useLocation();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    trigger,
+  } = useForm();
 
   // Función que maneja el evento de submit
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const handleSubmitForm = (e) => {
+  //   e.preventDefault();
 
-    if (user.isLogged) {
-      setUser({
-        ...user,
-        isLogged: false,
-        role: "user",
-      });
-      navigate("/");
-    } else if (!user.isLogged) {
-      if (!form.name || !form.email) {
-        alert("Por favor, rellena todos los campos");
-        return;
-      }
+  //   // if (user.isLogged) {
+  //   //   setUser({
+  //   //     ...user,
+  //   //     isLogged: false,
+  //   //     role: "user",
+  //   //   });
+  //   //   navigate("/");
+  //   // } else if (!user.isLogged) {
+  //   //   if (!form.name || !form.email) {
+  //   //     alert("Por favor, rellena todos los campos");
+  //   //     return;
+  //   // }
 
-      const userEmail = form.email.trim();
-      const role = userEmail.includes("@admin") ? "admin" : "user";
+  //   //   const userEmail = form.email.trim();
+  //   //   const role = userEmail.includes("@admin") ? "admin" : "user";
 
-      setUser({
-        ...user,
-        ...form,
-        isLogged: true,
-        role,
-      });
-      reset();
-      navigate(location.state?.pathname);
-    }
-  };
+  //   //   setUser({
+  //   //     ...user,
+  //   //     ...form,
+  //   //     isLogged: true,
+  //   //     role,
+  //   //   });
+  //   //   reset();
+  //   //   navigate(location.state?.pathname);
+  //   // }
+  // };
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
 
   return (
     <form
-      onSubmit={handleSubmit}
+      onSubmit={onSubmit}
       className="login-form"
     >
       {!user.isLogged && (
         <>
           <label htmlFor="name">Nombre</label>
+          {errors.name && <p>{errors.name.message}</p>}
           <input
             type="text"
-            name="name"
             id="name"
-            value={form.name}
-            onChange={(e) => setName(e.target.value)}
+            {...register("name", { required: "Por favor, ingrese su nombre" })}
           />
           <label htmlFor="email">Email</label>
+          {errors.email && <p>{errors.email.message}</p>}
           <input
             type="email"
-            name="email"
             id="email"
-            value={form.email}
-            onChange={(e) => setEmail(e.target.value)}
+            {...register("email", {
+              required: "Por favor, ingrese su email",
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                message: "El mail no es válido",
+              },
+            })}
+          />
+          <label htmlFor="password">Contraseña</label>
+          {errors.password && <p>{errors.password.message}</p>}
+          <input
+            type="password"
+            id="password"
+            {...register("password", {
+              required: "Minimo 8 caracteres",
+              minLength: { value: 8, message: "Minimo 8 caracteres" },
+            })}
+          />
+          <label htmlFor="confirmPassword">Repite la contraseña</label>
+          {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+          <input
+            type="password"
+            id="confirmPassword"
+            {...register("confirmPassword", {
+              required: true,
+              validate: (value) =>
+                value === watch("password") || "Las contraseñas no coinciden",
+            })}
           />
         </>
       )}
