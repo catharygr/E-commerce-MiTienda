f934ed41f99adfab96c7b3aff5839a2583ce3464
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-13 12:00:17 +0000

    Instalación de firebase

---
+++ react-lab1/src/api/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgjilMMJRvi2J6A7UWJ1HlPytfgw-SEZ8",
+  authDomain: "e-commerce-mi-tienda.firebaseapp.com",
+  projectId: "e-commerce-mi-tienda",
+  storageBucket: "e-commerce-mi-tienda.appspot.com",
+  messagingSenderId: "800543410493",
+  appId: "1:800543410493:web:c3d3b6b8b91fcf8f701d54",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
