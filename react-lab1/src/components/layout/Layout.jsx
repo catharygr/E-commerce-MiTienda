52f5c2da07dc83ee9aa1ac73b7baa83b14940fd0
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-14 10:21:40 +0000

    Trabajando en auth en Layout.jsx

--- react-lab1/src/components/layout/Layout.jsx
+++ react-lab1/src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "../footer/Footer";
 import { Outlet } from "react-router-dom";
 import Promotion from "../promotion/Promotion";
 import Loader from "../loader/Loader";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Error from "../error/Error";
 import {
@@ -12,24 +12,29 @@ import {
   getProductsError,
   getProductsThunk,
 } from "../../redux/reducers/productsReducer";
-
-// import * as oldData from "../../data/db copy.json";
-// import { db } from "../../api/firebase";
-// import { doc, setDoc } from "firebase/firestore";
-
-// oldData.products.forEach(async (product) => {
-//   const caty = product.id.toString();
-//   await setDoc(doc(db, "products", caty), product);
-// });
+import { auth } from "../../api/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { UserContext } from "../../contextos/UserContext";
 
 export default function Layout() {
   const dispatch = useDispatch();
   const loading = useSelector(getProductsLoading);
   const error = useSelector(getProductsError);
+  const { user, setUser } = useContext(UserContext);
 
   useEffect(() => {
     dispatch(getProductsThunk());
   }, []);
+
+  onAuthStateChanged(auth, (userAuth) => {
+    if (userAuth) {
+      setUser({ ...user, isLogged: true });
+      // console.log("User is signed in");
+    } else {
+      setUser({ isLogged: false });
+      // console.log("User is signed out");
+    }
+  });
 
   return (
     <>
