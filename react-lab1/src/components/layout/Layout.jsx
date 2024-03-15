f36f19c7f4ef415d3095bc2c879408d6241a29ea
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-15 13:04:33 +0000

    Limpiando el código

--- react-lab1/src/components/layout/Layout.jsx
+++ react-lab1/src/components/layout/Layout.jsx
@@ -13,6 +13,18 @@ import {
   getProductsThunk,
 } from "../../redux/reducers/productsReducer";
 
+// Este código se usó para traladar la data de un archivo JSON a Firestore
+
+// import * as oldData from "../../data/db-backup.json";
+// import { db } from "../../api/firebase";
+// import { doc, setDoc } from "firebase/firestore";
+
+// Iterate over the products array
+// oldData.products.forEach(async (product) => {
+//   const string = product.id.toString();
+//   await setDoc(doc(db, "products", string), product);
+// });
+
 export default function Layout() {
   const dispatch = useDispatch();
   const loading = useSelector(getProductsLoading);
